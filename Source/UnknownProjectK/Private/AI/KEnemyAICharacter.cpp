a28737adcf2da843f0f962c967c034479d711285
Pchooo <arina.kormschikova@gmail.com>
2023-12-05 23:20:43 +0300

    Added smooth rotation for enemy

--- Source/UnknownProjectK/Private/AI/KEnemyAICharacter.cpp
+++ Source/UnknownProjectK/Private/AI/KEnemyAICharacter.cpp
@@ -1,11 +1,17 @@
 #include "AI/KEnemyAICharacter.h"
 #include "AI/KEnemyAIController.h"
-
+#include "GameFramework/CharacterMovementComponent.h"
 
 AKEnemyAICharacter::AKEnemyAICharacter()
 {
     AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
     AIControllerClass = AKEnemyAIController::StaticClass();
+
+    if(GetCharacterMovement()) {
+        GetCharacterMovement()->bUseControllerDesiredRotation = true;
+        GetCharacterMovement()->RotationRate = FRotator(0,200,0);
+    }
+    bUseControllerRotationYaw = false; 
 }
 
 
